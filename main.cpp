abfe8b0fdfed273ed870a0469572c543b7fa6cde
Demetrey <just.t.m.o.p.i.t.w@gmail.com>
2021-10-07 15:05:46 +0300

    Timeout bug fix

--- main.cpp
+++ main.cpp
@@ -5,67 +5,75 @@
 #include <string.h>
 #include <unistd.h>
 #include <signal.h>
-#include <string>
 #include <iostream>
 
-int server_socket = 0;
+int server_socket = 0;                      // global for CTRL+C handler
 
-int data_exchange();
-void close_handler(int signal);
+int data_exchange();                        // get/send data
+void close_handler(const int signal);       // CTRL+C signal descriptor
 
+// [1] - port
 int main(int argc, char **argv) {
     signal(SIGINT, close_handler);
     int max_connection = 10;
-    if (argc != 2) {
-        std::cerr << "No arguments please enter socket argument";
-        return -1;
-    }
+    int timeout_in_secosnds = 60;
     struct sockaddr_in server_address;
+    if (argc != 2) {
+        perror("No arguments please enter socket argument");
+        return 1;
+    }
     server_socket = socket(AF_INET, SOCK_STREAM, 0);
     if (server_socket < 0) {
-        std::cerr << "Socket creation error" << std::endl;
-        return -2;
+        perror("Socket creation error");
+        return 2;
     }
     server_address.sin_family = AF_INET;
-    server_address.sin_port = htons(atoi(argv[1])); //argv[1]
+    server_address.sin_port = htons(atoi(argv[1]));
     server_address.sin_addr.s_addr = htonl(INADDR_ANY);
-    if (bind(server_socket, reinterpret_cast<sockaddr*>(&server_address), sizeof(server_address)) < 0) {
+    if (bind(server_socket, reinterpret_cast<sockaddr*>(&server_address), 
+            sizeof(server_address)) < 0) {
         perror("Socket binding error");
-        exit(2);
+        return 3;
     }
     listen(server_socket, max_connection);
+    // timeout {timeout_in_secosnds} sec
+    struct timeval tv {static_cast<time_t>(timeout_in_secosnds), 0};
+    setsockopt(server_socket, SOL_SOCKET, SO_RCVTIMEO, 
+                reinterpret_cast<const char*>(&tv), sizeof tv);
     return data_exchange();
 }
 
 int data_exchange() {
-    int current_connection = 0;
+    int current_connection;
     int bytes_read;
     const int SIZE = 1024;
     char buf[SIZE];
     struct sockaddr_in sender_address;
     socklen_t sender_len;
-    std::string answer = "TCP SERVER ANSWER";
-
-    fd_set fdset;
-    struct timeval tv {10, 0}; // timeout 10 sec
+    char answer[] = "TCP SERVER ANSWER";
     std::cout << "Server started" << std::endl;
     while (1) {
-        current_connection = accept(server_socket, reinterpret_cast<sockaddr*>(&sender_address), &sender_len);
+        memset(buf, 0, sizeof buf);
+        current_connection = accept(server_socket,
+                                reinterpret_cast<sockaddr*>(&sender_address), 
+                                &sender_len);
         if (current_connection == -1)
             continue;
         std::cout << "Connection!" << std::endl;
-        setsockopt(server_socket, SOL_SOCKET, SO_RCVTIMEO, (const char*)&tv, sizeof tv);
-
-        bytes_read = recv(current_connection, buf, sizeof(buf) - 1, 0); //recv(current_connection, buf, sizeof(buf) - 1, 0); //read(current_connection, buf, sizeof(buf) - 1)
+        // waiting for data transfer {timeout_in_seconds} seconds
+        bytes_read = recv(current_connection, buf, sizeof(buf) - 1, 0);
         if (bytes_read == -1) {
             perror("Error getting data");
         }
-        buf[bytes_read] = '\0';
+        buf[bytes_read] = '\0'; // set \0 after message
         std::cout << "Client data: " << buf << std::endl;
-        std::cout << "Client IP: " << inet_ntoa(sender_address.sin_addr) << std::endl;
-        std::cout << "Client port: " << static_cast<int>(ntohs(sender_address.sin_port)) << std::endl;
-
-        snprintf(buf, SIZE, "SERVER: %s", answer.c_str());
+        std::cout << "Client IP: " << inet_ntoa(sender_address.sin_addr) 
+                    << std::endl;
+        std::cout << "Client port: " 
+                    << static_cast<int>(ntohs(sender_address.sin_port)) 
+                    << std::endl;
+        // server response
+        snprintf(buf, SIZE, "SERVER: %s", answer);
         write(current_connection, buf, strlen(buf));
         close(current_connection);
     }
@@ -76,6 +84,5 @@ int data_exchange() {
 void close_handler(int signal) {
     // free socket
     close(server_socket);
-    // exit
     exit(0);
 }
